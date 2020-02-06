d532f20aacc06c1b7922a9962e276276484d6e5f
alaa almasri <alaa_almasri1997@yahoo.com>
2020-02-06 10:11:02 +0200

    final commit

--- js/author.js
+++ js/author.js
@@ -2,13 +2,14 @@
 function author()
 {
     
-    var author=Array('William Shakespeare','Emily Dickinson',' H. P. Lovecraft','Leo Tolstoy');
+    var authors=window.prompt("whats your author name");
+    var author=Array('William Shakespeare','Emily Dickinson',' H. P. Lovecraft','Leo Tolstoy','alaa');
     for(var i=0;i<=author.length;i++)
     {
         // alert("author name as been displayed in the console");
        // console.log(author[i]);
-
-        document.write("<h5>your faviourite author is</h5>" +" "+ author[i]);
+       if(author[i] === authors)
+        document.write("<h5 style='text-align:center;">your faviourite author is</h5>" +" "+ author[i]);
     }
 
 }
